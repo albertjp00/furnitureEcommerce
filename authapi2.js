bb8801bd0f1ef0cefac4f7ce1378252038aa1fa7
Albert <90371111+albertjp00@users.noreply.github.com>
2024-05-20 14:13:35 +0530

    Your commit message

--- authapi2.js
+++ authapi2.js
@@ -1,9 +1,10 @@
 const passport = require("passport")
+require('dotenv').config();
 
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
-const GOOGLE_CLIENT_ID = '310531477100-qa8g7vvom447smud8bico4bhuf5lkbpq.apps.googleusercontent.com'
-const GOOGLE_CLIENT_SECRET = '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'
+GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
